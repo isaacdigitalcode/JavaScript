4e8fe5d4f977ed23f23e8de3d686ed612063446c
Isaac Dantas <97792382+isaacdigitalcode@users.noreply.github.com>
2022-12-09 12:10:08 -0300

    atualização de exercícios 09/12/22

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('O consol funcionou corretamente!')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (cel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
