c9eacbf80507cf3ec6d1654317b21274c46849e7
Isaac Dantas <97792382+isaacdigitalcode@users.noreply.github.com>
2022-12-14 15:54:23 -0300

    conclusão do ex014

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,22 @@
-alert('Olá!')
+
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora =  data.getHours()
+    
+    msg.innerHTML = `<p><strong>Agora são ${hora} horas.</strong></p>`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'imagens/fotomanha.png'
+        document.body.style.background = '#baa689'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'imagens/fototarde.png'
+        document.body.style.background = '#a15a28'
+    } else {
+        //BOA NOITE!
+        img.src = 'imagens/fotonoite.png'
+        document.body.style.background = '#1e3033'
+    }
+}
