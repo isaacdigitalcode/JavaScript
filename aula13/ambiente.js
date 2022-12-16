ce7eb016ab1f2efa4738fefe31cd521686e15d9d
Isaac Dantas <97792382+isaacdigitalcode@users.noreply.github.com>
2022-12-16 10:27:20 -0300

    adicionado aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+//while == enquanto/ teste lógico 1º
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+//do == faça/ teste lógico após excutar o bloco
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+
+
+
+
